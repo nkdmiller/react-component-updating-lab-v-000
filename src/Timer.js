d4b68de126f0f28d78e31f7f4643a6d2078b50e3
Nate Miller <natekmiller96@gmail.com>
2018-11-09 20:17:11 +0000

    Done.

--- src/Timer.js
+++ src/Timer.js
@@ -12,8 +12,15 @@ class Timer extends Component {
   }
 
 
-
-  //Your code here
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.time === nextState.time) {
+      return false
+    }
+    return true
+  }
+  componentDidUpdate(){
+    this.timer.current.style.color = '#'+Math.floor(Math.random()*16777215).toString(16)
+  }
 
 
 
@@ -26,7 +33,7 @@ class Timer extends Component {
   }
 
   render() {
-    const { time, color, className, logText } = this.state
+    const { time, color, logText } = this.state
     return (
       <section className="Timer" style={{background: color}} ref={this.timer}>
 
